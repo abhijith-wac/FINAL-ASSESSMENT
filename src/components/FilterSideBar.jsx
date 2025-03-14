7d6729205caeac4432f498cc673578e6452a75dd
abhijith-wac <abhijith.v@webandcrafts.in>
2025-03-14 09:44:58 +0530

    final

--- src/components/FilterSideBar.jsx
+++ src/components/FilterSideBar.jsx
@@ -5,54 +5,60 @@ import SelectedFilters from "./SelectedFilters";
 import { useFilters } from "../customHooks/useFilter";
 
 const FilterSidebar = ({ filterList = [] }) => {
-    const { selectedFilters, handleCheckboxChange, handlePriceChange, searchParams, clearAllFilters, handleRemoveFilter } =
-        useFilters(filterList);
+  const {
+    selectedFilters,
+    handleCheckboxChange,
+    handlePriceChange,
+    searchParams,
+    clearAllFilters,
+    handleRemoveFilter,
+  } = useFilters(filterList);
 
-    const [expandedCategories, setExpandedCategories] = useState({});
+  const [expandedCategories, setExpandedCategories] = useState({});
 
-    const toggleShowMore = (category) => {
-        setExpandedCategories((prev) => ({
-            ...prev,
-            [category]: !prev[category],
-        }));
-    };
+  const toggleShowMore = (category) => {
+    setExpandedCategories((prev) => ({
+      ...prev,
+      [category]: !prev[category],
+    }));
+  };
 
-    return (
-        <div className="p-3 border rounded shadow-sm">
-            <h5 className="fw-bold mb-3">Filters</h5>
+  return (
+    <div className="p-3 border rounded shadow-sm">
+      <h5 className="fw-bold mb-3">Filters</h5>
 
-            <SelectedFilters
-                selectedFilters={selectedFilters}
-                handleRemoveFilter={handleRemoveFilter}
-                clearAllFilters={clearAllFilters}
-            />
+      <SelectedFilters
+        selectedFilters={selectedFilters}
+        handleRemoveFilter={handleRemoveFilter}
+        clearAllFilters={clearAllFilters}
+      />
 
-            <Accordion>
-                {filterList.map((filter) => (
-                    <Accordion.Item eventKey={filter.attribute} key={filter.attribute}>
-                        <Accordion.Header>
-                            {filter.label}
-                            {searchParams.getAll(filter.attribute).length > 0 && (
-                                <Badge bg="primary" className="ms-2">
-                                    {searchParams.getAll(filter.attribute).length}
-                                </Badge>
-                            )}
-                        </Accordion.Header>
-                        <Accordion.Body>
-                            <FilterOptions
-                                filter={filter}
-                                expanded={expandedCategories[filter.attribute]}
-                                toggleShowMore={() => toggleShowMore(filter.attribute)}
-                                searchParams={searchParams}
-                                handleCheckboxChange={handleCheckboxChange}
-                                handlePriceChange={handlePriceChange}
-                            />
-                        </Accordion.Body>
-                    </Accordion.Item>
-                ))}
-            </Accordion>
-        </div>
-    );
+      <Accordion>
+        {filterList.map((filter) => (
+          <Accordion.Item eventKey={filter.attribute} key={filter.attribute}>
+            <Accordion.Header>
+              {filter.label}
+              {searchParams.getAll(filter.attribute).length > 0 && (
+                <Badge bg="primary" className="ms-2">
+                  {searchParams.getAll(filter.attribute).length}
+                </Badge>
+              )}
+            </Accordion.Header>
+            <Accordion.Body>
+              <FilterOptions
+                filter={filter}
+                expanded={expandedCategories[filter.attribute]}
+                toggleShowMore={() => toggleShowMore(filter.attribute)}
+                searchParams={searchParams}
+                handleCheckboxChange={handleCheckboxChange}
+                handlePriceChange={handlePriceChange}
+              />
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
+    </div>
+  );
 };
 
 export default FilterSidebar;
